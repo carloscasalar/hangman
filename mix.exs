4b2592a084d9a0d8cf110e793b84a63e04d12df5
Carlos Castillo <castillo.st@gmail.com>
2019-10-27 13:13:14 +0100

    Add comment to remember how to clean dependency

--- mix.exs
+++ mix.exs
@@ -20,13 +20,14 @@ defmodule Hangman.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   # Write "mix deps.get" to retrieve dependencies
+  # Write mix deps.clean --unused --unlock
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # { :fetcher,  git: "https://repo.acme.com/fetcher", tag: "1.7" },
       # { :pusher,   github: "pragdave/pusher" },
-      { :dictionary, path: "../dictionary" },
+      {:dictionary, path: "../dictionary"},
     ]
   end
 end
