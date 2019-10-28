ba4bad2d9919fe1dbcfbfef188149361811690f9
Carlos Castillo <castillo.st@gmail.com>
2019-10-28 21:38:33 +0100

    Add make move method for win and lost

--- test/game_test.exs
+++ test/game_test.exs
@@ -15,4 +15,16 @@ defmodule GameTest do
     assert Enum.all? game.letters, fn letter -> letter =~ is_letter end
   end
 
+  test "state isn't changed for :won game" do
+    game = Game.init_game() |> Map.put(:game_state, :won)
+    assert { ^game, _} = Game.make_move(game, "x")
+  end
+
+
+  test "state isn't changed for :lost game" do
+    game = Game.init_game() |> Map.put(:game_state, :lost)
+    assert { ^game, _} = Game.make_move(game, "x")
+  end
+
+
 end
