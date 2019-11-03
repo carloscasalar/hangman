c9dda372d3da0eac1a747f40cd5565815499bf23
Carlos Castillo <castillo.st@gmail.com>
2019-11-03 18:48:14 +0100

    Make Game.make_move returns only a game

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -3,7 +3,11 @@ defmodule Hangman do
   alias Hangman.Game
 
   defdelegate new_game(), to: Game, as: :init_game
-  defdelegate make_move(game, guess), to: Game
   defdelegate tally(game), to: Game
+
+  def make_move(game, guess) do
+    game = Game.make_move(game, guess)
+    {game, Game.tally(game)}
+  end
 
 end
