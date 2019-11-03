168a0ada3dfb01abc2884f01bf80d8a59f8b79a7
Carlos Castillo <castillo.st@gmail.com>
2019-11-03 18:31:25 +0100

    Implement tally method and expose it in public API

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -3,4 +3,7 @@ defmodule Hangman do
   alias Hangman.Game
 
   defdelegate new_game(), to: Game, as: :init_game
+  defdelegate make_move(game, guess), to: Game
+  defdelegate tally(game), to: Game
+
 end
